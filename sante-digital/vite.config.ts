e07cc64a88c03fafa69e75eb0cebff40cf978c30
chouat jassem <chouatjasem@gmail.com>
2025-12-10 10:31:56 +0100

    Ajouter .env au gitignore

--- sante-digital/vite.config.ts
+++ sante-digital/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      build: {
+    outDir: 'dist' // ← important, c'est la valeur par défaut mais mieux vaut être explicite
+  },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
